cb62048456af3c729efe2e664a7c833945cdc61e
ivannapalcazar <ivannapalcazar@gmail.com>
2021-10-28 17:10:14 -0300

    subiendo codigo

---
+++ hola_mundo.py
@@ -0,0 +1,10 @@
+#esto es un comentario en python y a continuacion declarare variables
+var = "5"
+var_1 = "99"
+var_2 = True
+#estoy aprendiendo el tipo de dato asignado  una variable a traves de la palabra type
+print("el valor de var es")
+print(type(var))
+print("el valor de var_1 es")
+print(type(var_1))
+print(type(var_2))
